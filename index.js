a6ebdce254c58046a6cdb2e83c2a6a8ffbe65362
Add Chan <addison95132@gmail.com>
2015-05-13 12:37:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var index = function(){
   if(/(iPhone|iPod|iPad)/i.test(navigator.userAgent)) { 
     if(/OS [2-7]_\d(_\d)? like Mac OS X/i.test(navigator.userAgent)) {  
-        // iOS 2-7 so Do Something   
-        $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
+      // iOS 2-7 so Do Something   
+      $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
     } else if(/CPU like Mac OS X/i.test(navigator.userAgent)) {
-        // iOS 1 so Do Something 
-        $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
+      // iOS 1 so Do Something 
+      $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
     } else {
-        // iOS 8 or Newer so Do Nothing
+      // iOS 8 or Newer so Do Nothing
     }
-}
+  } else {
+    $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'normal.css') );
+  }
 }
 $(document).ready(index)
