54f5f1032396a824d4429da287c565c7f9d3af13
Add Chan <addison95132@gmail.com>
2015-05-13 19:32:46 -0700

    Update index.js

--- about_us/index.js
+++ about_us/index.js
@@ -1,4 +1,19 @@
-var start = function(){
-
+var index = function(){
+  var index = function(){
+  if(/(iPhone|iPod|iPad)/i.test(navigator.userAgent)) { 
+    if(/OS [2-7]_\d(_\d)? like Mac OS X/i.test(navigator.userAgent)) {  
+      // iOS 2-7 so Do Something   
+      $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
+    } else if(/CPU like Mac OS X/i.test(navigator.userAgent)) {
+      // iOS 1 so Do Something 
+      $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
+    } else {
+      // iOS 8 or Newer so Do Nothing
+    }
+  } else {
+    $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'normal.css') );
+  }
 }
-$(document).ready(start)
+$(document).ready(index)
+}
+$(document).ready(index)
