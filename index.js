919bfba4931977fb9072c7fc8724828d97381130
Add Chan <addison95132@gmail.com>
2015-05-15 07:17:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ if (ver!= undefined) {
 if (ver[0] <7) {
   console.log('ios7')
   $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'iOSFix.css') );
+} else {
+  console.log('computer')
+  $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'normal.css') );
 }} else {
   console.log('computer')
   $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'normal.css') );
